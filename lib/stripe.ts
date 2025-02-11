e06a4d183de53d6f841cce5bed368e0c7b1c0213
xndrbrgs <aborgescolon@gmail.com>
2025-02-11 18:52:08 -0500

    Stripe keys official

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+export const stripe = new Stripe(process.env.STRIPE_SK_OFFICIAL!, {
     apiVersion: "2025-01-27.acacia",
     typescript: true,
 })
