85cfc573d99bda6538732980ffcc376de3ee851b
xndrbrgs <aborgescolon@gmail.com>
2025-02-15 21:37:16 -0500

    trying vercels

--- app/api/stripe/connect/route.ts
+++ app/api/stripe/connect/route.ts
@@ -1,12 +1,10 @@
 
 import prisma from "@/lib/db";
 import { stripe } from "@/lib/stripe";
-import { headers } from "next/headers";
 
 export async function POST(req: Request) {
     const body = await req.text();
-
-    const signature = headers().get("Stripe-Signature") as string;
+    const signature = req.headers.get('stripe-signature') as string;
 
     let event;
 
