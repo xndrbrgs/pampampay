0b83e2c3de7de24b85ba091e82f9aaa4ba5b44b0
xndrbrgs <aborgescolon@gmail.com>
2025-02-18 16:24:48 -0500

    Auth-transfer corrected

--- app/(dashboard)/dashboard/auth-transfer/page.tsx
+++ app/(dashboard)/dashboard/auth-transfer/page.tsx
@@ -1,26 +1,68 @@
-import { AddConnection } from "@/components/General/AddConnections";
 import Header from "@/components/General/Header";
-import TransferTabs from "@/components/General/TransferTabs";
-import { addConnection } from "@/lib/actions/transfer.actions";
-import { createOrGetUser } from "@/lib/actions/user.actions";
-import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  createStripeAccountLink,
+  getStripeDashboard,
+} from "@/lib/actions/transfer.actions";
+import {
+  createOrGetUser,
+  getStripeConnnectUser,
+} from "@/lib/actions/user.actions";
 
-export default async function TransferPage() {
-   const user = await createOrGetUser();
-    if (user.stripeConnectedLinked === false) {
-      redirect("/dashboard");
-    }
-  
+const AuthTransfer = async () => {
+  const user = await createOrGetUser();
+  const data = await getStripeConnnectUser(user.clerkUserId);
   return (
     <section className="p-6 h-screen">
       <Header
-        title="Transfer Money"
-        subtext="Manage your transfers easily and safely."
+        title="Authorize Transfer"
+        subtext="Create your Stripe Connect account to begin accepting payments"
       />
-      <div className="flex flex-col gap-4 max-w-3xl">
-        <AddConnection onAddConnection={addConnection} />
-        <TransferTabs />
+      <div className="max-w-3xl">
+        {data?.stripeConnectedLinked === false && (
+          <div className="mt-5">
+            <Card className="card-style">
+              <CardHeader>
+                <CardTitle>Link Your Account to Stripe</CardTitle>
+                <CardDescription>
+                  Link your account to Stripe to begin receiving payments.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form action={createStripeAccountLink}>
+                  <Button type="submit">Click To Link Account</Button>
+                </form>
+              </CardContent>
+            </Card>
+          </div>
+        )}
+        {data?.stripeConnectedLinked === true && (
+          <div className="mt-5">
+            <Card className="card-style">
+              <CardHeader>
+                <CardTitle>View Your Account Dashboard</CardTitle>
+                <CardDescription>
+                  View your Stripe account dashboard for more payment details.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form action={getStripeDashboard}>
+                  <Button type="submit">Click To View Dashboard</Button>
+                </form>
+              </CardContent>
+            </Card>
+          </div>
+        )}
       </div>
     </section>
   );
-}
+};
+
+export default AuthTransfer;
--- app/(dashboard)/dashboard/pay-and-request/page.tsx
+++ app/(dashboard)/dashboard/pay-and-request/page.tsx
@@ -1,7 +1,16 @@
+import { AddConnection } from "@/components/General/AddConnections";
 import Header from "@/components/General/Header";
 import TransferTabs from "@/components/General/TransferTabs";
+import { addConnection } from "@/lib/actions/transfer.actions";
+import { createOrGetUser } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
 
-export default function TransferPage() {
+export default async function TransferPage() {
+   const user = await createOrGetUser();
+    if (user.stripeConnectedLinked === false) {
+      redirect("/dashboard");
+    }
+  
   return (
     <section className="p-6 h-screen">
       <Header
@@ -9,6 +18,7 @@ export default function TransferPage() {
         subtext="Manage your transfers easily and safely."
       />
       <div className="flex flex-col gap-4 max-w-3xl">
+        <AddConnection onAddConnection={addConnection} />
         <TransferTabs />
       </div>
     </section>
