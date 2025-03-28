5e72c98609337e5885b27b6649a899b86a2ef426
Alex Borges <aborgescolon@gmail.com>
2025-03-27 23:09:58 -0400

    Changed Stripe webhook

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,74 +1,92 @@
-import prisma from "@/lib/db";
-import { stripe } from "@/lib/stripe";
+import { Stripe } from "stripe";
+import { NextResponse } from "next/server";
 import { headers } from "next/headers";
-import Cors from 'micro-cors';
+import prisma from "@/lib/db";
 
-Cors({
-    allowMethods: ['POST', 'HEAD'],
-});
+const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET as string);
 
 export async function POST(req: Request) {
-    const body = await req.text();
+  let event: Stripe.Event;
 
-    const signature = (await headers()).get("stripe-signature");
+  try {
+    const stripeSignature = (await headers()).get("stripe-signature");
 
-    if (!signature) {
-        console.error('Stripe-Signature header is missing.');
-        return new Response('Stripe webhook error: Missing signature', { status: 400 });
-    }
+    event = stripe.webhooks.constructEvent(
+      await req.text(),
+      stripeSignature as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    );
+  } catch (err) {
+    const errorMessage = err instanceof Error ? err.message : "Unknown error";
+    // On error, log and return the error message.
+    if (err! instanceof Error) console.log(err);
+    console.log(`❌ Error message: ${errorMessage}`);
+    return NextResponse.json(
+      { message: `Webhook Error: ${errorMessage}` },
+      { status: 400 }
+    );
+  }
 
-    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  // Successfully constructed event.
+  console.log("✅ Success:", event.id);
 
-    if (!webhookSecret) {
-        console.error('STRIPE_WEBHOOK_SECRET is not set.');
-        return new Response('Stripe webhook error: Missing webhook secret', { status: 500 });
-    }
+  const permittedEvents: string[] = [
+    "checkout.session.completed",
+    "payment_intent.succeeded",
+    "payment_intent.payment_failed",
+  ];
 
-    let event;
+  if (permittedEvents.includes(event.type)) {
+    let data;
 
     try {
-        event = stripe.webhooks.constructEvent(
-            body,
-            signature,
-            webhookSecret
-        );
+      switch (event.type) {
+        case "checkout.session.completed":
+          data = event.data.object as Stripe.Checkout.Session;
+          console.log(`💰 CheckoutSession status: ${data.payment_status}`);
 
-    } catch (error: any) {
-        console.error('Stripe webhook error:', error.message);
-        return new Response('Stripe webhook error', { status: 400 });
-
-    }
-
-    switch (event.type) {
-        case "checkout.session.completed": {
-            const session = event.data.object;
-
-            if (session.id) {
-                try {
-                    await prisma.transfer.update({
-                        where: {
-                            id: session.id
-                        },
-                        data: {
-                            status: 'COMPLETED',
-                        }
-                    });
-                    console.log(`Updated transfer status to COMPLETED for session ${session.id}`);
-                } catch (prismaError: any) {
-                    console.error('Prisma update error:', prismaError.message);
-                    // Consider a more specific error response if the update fails
-                }
-            } else {
-                console.warn('Session ID is missing in the event data.');
+          if (data.id) {
+            try {
+              await prisma.transfer.update({
+                where: {
+                  id: data.id,
+                },
+                data: {
+                  status: "COMPLETED",
+                },
+              });
+              console.log(
+                `Updated transfer status to COMPLETED for session ${data.id}`
+              );
+            } catch (prismaError: any) {
+              console.error("Prisma update error:", prismaError.message);
+              // Consider a more specific error response if the update fails
             }
+          } else {
+            console.warn("Session ID is missing in the event data.");
+          }
 
-            break;
-        }
-        default: {
-            console.log('unhandled event', event.type, event);
-        }
+          break;
+        case "payment_intent.payment_failed":
+          data = event.data.object as Stripe.PaymentIntent;
+          console.log(`❌ Payment failed: ${data.last_payment_error?.message}`);
+          break;
+        case "payment_intent.succeeded":
+          data = event.data.object as Stripe.PaymentIntent;
+          console.log(`💰 PaymentIntent status: ${data.status}`);
+          break;
+        default:
+          throw new Error(`Unhandled event: ${event.type}`);
+      }
+    } catch (error) {
+      console.log(error);
+      return NextResponse.json(
+        { message: "Webhook handler failed" },
+        { status: 500 }
+      );
     }
+  }
 
-
-    return new Response(null, { status: 200 });
+  // Return a response to acknowledge receipt of the event.
+  return NextResponse.json({ message: "Received" }, { status: 200 });
 }
