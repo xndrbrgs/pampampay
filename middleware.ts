d5db4b5b8f310049abd8f16ab66d4c0dcb9080d3
Alex Borges <aborgescolon@gmail.com>
2025-04-02 15:28:52 -0400

    changed middleware

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,25 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
-import { NextResponse } from "next/server"
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const publicPaths = ["/", "/api/stripe", "/api/stripe/connect", "/api/webhook"]
+const publicPaths = [
+  "/",
+  "/api/stripe",
+  "/api/stripe/connect",
+  "/api/webhooks",
+  "/api/payments",
+  "/api/payments/coinbase",
+  "/api/payments/coinbase/success",
+  "/api/payments/coinbase/cancel",
+];
 
-const isPublic = createRouteMatcher(publicPaths)
+const isPublic = createRouteMatcher(publicPaths);
 
 export default clerkMiddleware(async (auth, request) => {
   if (isPublic(request)) {
-    return NextResponse.next()
+    return NextResponse.next();
   }
-})
+});
 
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-}
-
+};
