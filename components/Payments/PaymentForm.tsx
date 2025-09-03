695d3e38276de23df3b0b11a18cdca96f80e2677
xndrbrgs <aborgescolon@gmail.com>
2025-09-03 18:22:22 -0400

    adding 5 for video

--- components/Payments/PaymentForm.tsx
+++ components/Payments/PaymentForm.tsx
@@ -372,7 +372,7 @@ export function GeneralPaymentForm({
                               <SelectValue placeholder="Select an amount" />
                             </SelectTrigger>
                             <SelectContent>
-                              {[20, 25, 30, 40, 50, 60, 70, 80, 90, 100].map(
+                              {[5, 20, 25, 30, 40, 50, 60, 70, 80, 90, 100].map(
                                 (value) => (
                                   <SelectItem
                                     key={value}
